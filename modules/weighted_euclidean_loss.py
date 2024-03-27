25eec6086803efb4fb3ed1bb7ba44d462166698a
Yuyan-C <yuyan.chen2@mail.mcgill.ca>
2024-03-26 21:19:43 -0400

    change device

--- modules/weighted_euclidean_loss.py
+++ modules/weighted_euclidean_loss.py
@@ -11,20 +11,32 @@ class WeightedEuclideanLossLayer(torch.autograd.Function):
         weightNeg = np.zeros_like(crop.cpu().data, dtype=np.float32)
 
         distL1 = crop.data - flux.data
-        distL2 = distL1 ** 2
+        distL2 = distL1**2
         # the amount of positive and negative pixels
-        regionPos = (dilmask.data > 0)
-        regionNeg = (dilmask.data == 0)
+        regionPos = dilmask.data > 0
+        regionNeg = dilmask.data == 0
         sumPos = regionPos.sum()
         sumNeg = regionNeg.sum()
         # balanced weight for positive and negative pixels
-        weightPos[0][0] = sumNeg.float() / float(sumPos + sumNeg) * regionPos.cpu().float()
-        weightPos[0][1] = sumNeg.float() / float(sumPos + sumNeg) * regionPos.cpu().float()
-        weightNeg[0][0] = sumPos.float() / float(sumPos + sumNeg) * regionNeg.cpu().float()
-        weightNeg[0][1] = sumPos.float() / float(sumPos + sumNeg) * regionNeg.cpu().float()
+        weightPos[0][0] = (
+            sumNeg.float() / float(sumPos + sumNeg) * regionPos.cpu().float()
+        )
+        weightPos[0][1] = (
+            sumNeg.float() / float(sumPos + sumNeg) * regionPos.cpu().float()
+        )
+        weightNeg[0][0] = (
+            sumPos.float() / float(sumPos + sumNeg) * regionNeg.cpu().float()
+        )
+        weightNeg[0][1] = (
+            sumPos.float() / float(sumPos + sumNeg) * regionNeg.cpu().float()
+        )
         # total loss
-        loss = (distL2 * torch.from_numpy(weightPos + weightNeg).cuda()).sum() / len(crop) / 2. / (
-                    weightPos + weightNeg).sum()
+        loss = (
+            (distL2 * torch.from_numpy(weightPos + weightNeg).cuda()).sum()
+            / len(crop)
+            / 2.0
+            / (weightPos + weightNeg).sum()
+        )
         ctx.save_for_backward(crop, flux, dilmask)
         return loss
 
@@ -35,18 +47,25 @@ class WeightedEuclideanLossLayer(torch.autograd.Function):
         weightNeg = np.zeros_like(crop.cpu().data, dtype=np.float32)
         distL1 = crop.data - flux.data
         # the amount of positive and negative pixels
-        regionPos = (dilmask.data > 0)
-        regionNeg = (dilmask.data == 0)
-        sumPos = regionPos.sum()
-        sumNeg = regionNeg.sum()
+        regionPos = dilmask.data > 0
+        regionNeg = dilmask.data == 0
+        sumPos = regionPos.sum().cpu()
+        sumNeg = regionNeg.sum().cpu()
         # balanced weight for positive and negative pixels
-        weightPos[0][0] = sumNeg.float() / float(sumPos + sumNeg) * regionPos.cpu().float()
-        weightPos[0][1] = sumNeg.float() / float(sumPos + sumNeg) * regionPos.cpu().float()
-        weightNeg[0][0] = sumPos.float() / float(sumPos + sumNeg) * regionNeg.cpu().float()
-        weightNeg[0][1] = sumPos.float() / float(sumPos + sumNeg) * regionNeg.cpu().float()
+        weightPos[0][0] = (
+            sumNeg.float() / float(sumPos + sumNeg) * regionPos.cpu().float()
+        )
+        weightPos[0][1] = (
+            sumNeg.float() / float(sumPos + sumNeg) * regionPos.cpu().float()
+        )
+        weightNeg[0][0] = (
+            sumPos.float() / float(sumPos + sumNeg) * regionNeg.cpu().float()
+        )
+        weightNeg[0][1] = (
+            sumPos.float() / float(sumPos + sumNeg) * regionNeg.cpu().float()
+        )
         grad1 = distL1 * torch.from_numpy(weightPos + weightNeg).cuda() / len(crop)
         return grad1 * grad_output, None, None
 
 
 wel = WeightedEuclideanLossLayer.apply
-
